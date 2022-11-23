612f228f1bb85e456e29bc102d99fa4b8311f092
Nikola Vlaškalin <vlaskalin.ra187.2018@uns.ac.rs>
2022-11-23 02:31:03 +0100

    Added model 2. Under construction!!

---
+++ projektniZadatak_m2/main.cpp
@@ -0,0 +1,204 @@
+
+#include <stdlib.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include "WAVheader.h"
+#include "processing.h"
+
+
+
+// IO Buffers
+static double sampleBuffer[MAX_NUM_CHANNEL][BLOCK_SIZE];
+
+
+
+
+
+/////////////////////////////////////////////////////////////////////////////////
+// @Author	Nikola Vlaskalin
+// @Date		21.11.2022. 
+//
+// Function:
+// main
+//
+// @param - argv[0] - Input file name
+//        - argv[1] - Output file name
+// @return - nothing
+// Comment: main routine of a program
+//
+// E-mail:	nikola.vlaskalin00@gmail.com
+//
+/////////////////////////////////////////////////////////////////////////////////
+int main(int argc, char* argv[])
+{
+	FILE* wav_in = NULL;
+	FILE* wav_out = NULL;
+	char WavInputName[256];
+	char WavOutputName[256];
+	WAV_HEADER inputWAVhdr, outputWAVhdr;
+	// TO DO: Check if I should use global variables from processing.
+	double input_gain;
+	double input_gainDB;
+	double headroom_gain;
+	double headroom_gainDB;
+	int mode;
+	int OUTPUT_NUM_CHANNELS;
+
+	if (argc < 3 || argc > 6 || argc == 4 || argc == 5)
+	{
+		printf("Wrong input.\n");
+		printf("Command line arguments: \n");
+		printf("[Input file location] [Output file location] [Input Gain] [Headroom Gain] [Mode] \n");
+		printf("\n");
+		printf("a) Input gain -> Default = -6db Values: From 0 to  negative inf db \n");
+		printf("b) Headroom gain -> Default = -6db Values: From 0 to negative inf db \n");
+		printf("c) Mode: 0 -> OM2_0_0, 1 -> OM0_2_0, 2 -> OM3_2_0 \n");
+		return -1;
+	}
+	if (argc == 3)
+	{
+		input_gain = MINUS_6DB;
+		headroom_gain = MINUS_3DB;
+		mode = OM2_0_0;
+		OUTPUT_NUM_CHANNELS = 2;
+	} else
+	{
+		//TO DO: Should I add if input_gain > 1 / 2
+		input_gainDB = atof(argv[3]);
+		input_gainDB = input_gainDB > 0 ? -6.0 : input_gainDB;
+		input_gain = pow(10.0, input_gainDB / 20.0);
+
+		headroom_gainDB = atof(argv[4]);
+		headroom_gainDB = headroom_gainDB > 0 ? -3.0 : headroom_gainDB;
+		headroom_gain = pow(10.0, headroom_gainDB / 20.0);
+
+		mode = atoi(argv[5]);
+
+		if (mode < 0 || mode > 3)
+		{
+			mode = OM2_0_0;
+			OUTPUT_NUM_CHANNELS = 2;
+		}
+
+		switch (mode)
+		{
+		case OM2_0_0:
+			OUTPUT_NUM_CHANNELS = 2;
+			break;
+		case  OM0_2_0:
+			OUTPUT_NUM_CHANNELS = 2;
+			break;
+		case OM3_2_0:
+			OUTPUT_NUM_CHANNELS = 5;
+			break;
+		default:
+			break;
+		}
+	}
+
+	// Init channel buffers
+	for (int i = 0; i < MAX_NUM_CHANNEL; i++)
+		memset(&sampleBuffer[i], 0, BLOCK_SIZE);
+
+	// Open input and output wav files
+	//-------------------------------------------------
+	strcpy(WavInputName, argv[1]);
+	wav_in = OpenWavFileForRead(WavInputName, "rb");
+	strcpy(WavOutputName, argv[2]);
+	wav_out = OpenWavFileForRead(WavOutputName, "wb");
+	//-------------------------------------------------
+	// Read input wav header
+	//-------------------------------------------------
+	ReadWavHeader(wav_in, inputWAVhdr);
+	//-------------------------------------------------
+
+
+
+	//Default values
+
+
+	// Set up output WAV header
+	//-------------------------------------------------	
+	outputWAVhdr = inputWAVhdr;
+	outputWAVhdr.fmt.NumChannels = OUTPUT_NUM_CHANNELS; // change number of channels
+
+	int oneChannelSubChunk2Size = inputWAVhdr.data.SubChunk2Size / inputWAVhdr.fmt.NumChannels;
+	int oneChannelByteRate = inputWAVhdr.fmt.ByteRate / inputWAVhdr.fmt.NumChannels;
+	int oneChannelBlockAlign = inputWAVhdr.fmt.BlockAlign / inputWAVhdr.fmt.NumChannels;
+
+	outputWAVhdr.data.SubChunk2Size = oneChannelSubChunk2Size * outputWAVhdr.fmt.NumChannels;
+	outputWAVhdr.fmt.ByteRate = oneChannelByteRate * outputWAVhdr.fmt.NumChannels;
+	outputWAVhdr.fmt.BlockAlign = oneChannelBlockAlign * outputWAVhdr.fmt.NumChannels;
+
+
+	// Write output WAV header to file
+	//-------------------------------------------------
+	WriteWavHeader(wav_out, outputWAVhdr);
+	// Added initialize function for values.
+	initialize(input_gain, headroom_gain, mode);
+
+	// Processing loop
+	//-------------------------------------------------	
+	{
+		int sample;
+		int BytesPerSample = inputWAVhdr.fmt.BitsPerSample / 8;
+		const double SAMPLE_SCALE = -(double)(1 << 31);		//2^31
+		int iNumSamples = inputWAVhdr.data.SubChunk2Size / (inputWAVhdr.fmt.NumChannels * inputWAVhdr.fmt.BitsPerSample / 8);
+
+		// exact file length should be handled correctly...
+		for (int i = 0; i < iNumSamples / BLOCK_SIZE; i++)
+		{
+			for (int j = 0; j < BLOCK_SIZE; j++)
+			{
+				for (int k = 0; k < inputWAVhdr.fmt.NumChannels; k++)
+				{
+					sample = 0; //debug
+					fread(&sample, BytesPerSample, 1, wav_in);
+					sample = sample << (32 - inputWAVhdr.fmt.BitsPerSample); // force signextend
+					sampleBuffer[k][j] = sample / SAMPLE_SCALE;				// scale sample to 1.0/-1.0 range		
+				}
+			}
+
+			gainProcessing(sampleBuffer, sampleBuffer);
+
+			for (int j = 0; j < BLOCK_SIZE; j++)
+			{
+				for (int k = 0; k < outputWAVhdr.fmt.NumChannels; k++)
+				{
+					int channel = 0;
+					switch (mode)
+					{
+					case OM2_0_0:
+						if (k == 0) channel = LEFT_CH;
+						if (k == 1) channel = RIGHT_CH;
+						break;
+					case OM0_2_0:
+						if (k == 0) channel = LEFTS_CH;
+						if (k == 1) channel = RIGHTS_CH;
+						break;
+					case OM3_2_0:
+						if (k == 0) channel = LEFT_CH;
+						if (k == 1) channel = RIGHT_CH;
+						if (k == 2) channel = CENTER_CH;
+						if (k == 3) channel = LEFTS_CH;
+						if (k == 4) channel = RIGHTS_CH;
+					default:
+						break;
+					}
+					sample = sampleBuffer[channel][j] * SAMPLE_SCALE;	// crude, non-rounding 			
+					sample = sample >> (32 - inputWAVhdr.fmt.BitsPerSample);
+					fwrite(&sample, outputWAVhdr.fmt.BitsPerSample / 8, 1, wav_out);
+				}
+			}
+		}
+	}
+
+	// Close files
+	//-------------------------------------------------	
+	fclose(wav_in);
+	fclose(wav_out);
+	//-------------------------------------------------	
+
+	return 0;
+}
---
+++ projektniZadatak_m2/processing.cpp
@@ -0,0 +1,110 @@
+#include "common.h"
+#include "processing.h"
+
+/*double saturation(double in, double threshold)
+{
+	// Simple limiter since we know that pre-Gain adds 6dB
+	if (in > threshold)
+	{
+		return fmin(in, threshold);
+	}
+	else if (in < -threshold)
+	{
+		return fmax(in, -threshold);
+	}
+
+	return in;
+}*/
+static double tremoloBuffer[TREMOLO_NUM_CHANNELS][BLOCK_SIZE];
+double input_gain;
+double headroom_gain;
+int mode;
+tremolo_struct_t tremolo;
+tremolo_struct_t* tremolo_ptr = &tremolo;
+
+void initialize(double input_gain_func, double headroom_gain_func, int mode_func) 
+{
+	input_gain = input_gain_func;
+	headroom_gain = headroom_gain_func;
+	mode = mode_func;
+}
+
+
+void gainProcessing(double pIn[][BLOCK_SIZE], double pOut[][BLOCK_SIZE])
+{
+	double sum;
+
+	init(tremolo_ptr);
+
+	double* p_in_left = *(pIn + LEFT_CH);
+	double* p_in_right = *(pIn + RIGHT_CH);
+
+	double* p_in_tempL = *(tremoloBuffer + LEFT_CH);
+	double* p_in_tempR = *(tremoloBuffer + RIGHT_CH);
+
+	double* p_out_left = *(pOut + LEFT_CH);
+	double* p_out_right = *(pOut + RIGHT_CH);
+	double* p_out_center = *(pOut + CENTER_CH);
+	double* p_out_LS = *(pOut + LEFTS_CH);
+	double* p_out_RS = *(pOut + RIGHTS_CH);
+
+	for (int i = 0; i < BLOCK_SIZE; i++)
+	{	
+
+		// Multiply with -6db
+		*p_in_left *= input_gain;
+		*p_in_right *= input_gain;
+
+		*p_in_tempL = *p_in_left;
+		*p_in_tempR = *p_in_right;
+
+		if (mode != OM0_2_0)
+		{
+			// Sum of left and right channel and multiply the sum with -3db
+			sum = (*p_in_left + *p_in_right) * headroom_gain;
+
+			// Output for left, right and center channel
+
+			*p_out_left = sum * MINUS_6DB;
+			*p_out_right = sum * MINUS_6DB;
+		}
+
+		if (mode == OM3_2_0)
+		{
+			*p_out_center = sum;
+			p_out_center++;
+		}
+		p_in_left++;
+		p_in_right++;
+		p_in_tempL++;
+		p_in_tempR++;
+		p_out_left++;
+		p_out_right++;
+	}
+
+	// Tremolo processing for Ls and Rs
+	// void processBlock(double* input, double* output, tremolo_struct_t* data, int numSamples);
+
+	if (mode != OM2_0_0)
+	{
+		p_in_tempL = *(tremoloBuffer + LEFT_CH);
+		p_in_tempR = *(tremoloBuffer + RIGHT_CH);
+
+		processBlock(p_in_tempL, p_in_tempL, tremolo_ptr);
+		init(tremolo_ptr);
+		processBlock(p_in_tempR, p_in_tempR, tremolo_ptr);
+
+
+		for (int i = 0; i < BLOCK_SIZE; i++)
+		{
+			*p_out_LS = *p_in_tempL * MINUS_2DB;
+			*p_out_RS = *p_in_tempR * MINUS_2DB;
+
+
+			p_in_tempL++;
+			p_in_tempR++;
+			p_out_LS++;
+			p_out_RS++;
+		}
+	}
+}
